53083cef8a85aeba12c81180374025584be4eabd
BeardBear <138500494+BeardBear33@users.noreply.github.com>
2025-11-18 03:42:42 +0100

    Change sql folder

--- sql/world/backup/token_revert.sql
+++
@@ -1,7 +0,0 @@
--- Vrácení změn itemu 37711 (původní hodnoty)
-UPDATE item_template
-SET name = 'Currency Token Test Token 1',
-    description = 'A test token for the Currency Token system',
-    maxcount = 100,
-    stackable = 100
-WHERE entry = 37711;
